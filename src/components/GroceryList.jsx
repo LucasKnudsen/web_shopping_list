67091a5d4d525ce44c2b8935c710cf2539b4b8f7
Lucas Knudsen <laksteelhouse@gmail.com>
2021-04-14 19:54:29 +0200

    Adds rerender functionality on CRUD actions

--- src/components/GroceryList.jsx
+++ src/components/GroceryList.jsx
@@ -2,19 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { fetchGroceries, addGrocery, deleteGrocery, } from '../modules/groceryServices'
 import Checkbox from '@material-ui/core/Checkbox';
 import Delete from '@material-ui/icons/DeleteOutlined'
+import Icon from '@material-ui/core/Icon';
+import { IconButton, TextField } from '@material-ui/core';
 
 const GroceryList = () => {
   const [groceries, setGroceries] = useState([])
   const [input, setInput] = useState()
+  const [update, setUpdate] = useState(false)
 
   useEffect(() => {
     fetchGroceries(setGroceries)
-  }, [])
+  }, [update])
+
+  const addNewItem = async () => {
+    await addGrocery(input)
+    setUpdate(!update)
+  }
+
+  const deleteItem = async (id) => {
+    await deleteGrocery(id)
+    setUpdate(!update)
+  }
 
   return (
-
     <>
-
+      <form style={styles.input}>
+        <TextField color='secondary' label="Hvaaa skal vi have?" value={input} onChange={(e) => setInput(e.target.value)} />
+        <IconButton color='secondary' onClick={addNewItem}>
+          <Icon>add_circle</Icon>
+        </IconButton>
+      </form>
       <ul style={styles.listContainer}>
         {groceries.map(item => {
           return (
@@ -23,10 +40,10 @@ const GroceryList = () => {
                 <Checkbox />
                 <p>{item.name}</p>
               </div>
-              <Delete style={{ padding: 10, cursor: 'pointer' }} onClick={() => deleteGrocery(item.id)} />
+              <Delete style={{ padding: 10, cursor: 'pointer' }} onClick={() => deleteItem(item.id)} />
             </li>
           )
-        })
+        }).reverse()
         }
       </ul>
     </>
@@ -48,5 +65,10 @@ const styles = {
     margin: '7px 10px',
     borderRadius: 5,
     justifyContent: 'space-between'
+  },
+  input: {
+    textAlign: 'center',
+    margin: '25px 0'
+
   }
 }
